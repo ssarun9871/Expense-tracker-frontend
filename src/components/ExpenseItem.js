5afee0a497d2b3c5e0c7a701a30a4b73180e558d
ssarun9871 <ssarun9871@gmail.com>
2023-09-04 01:16:44 +0530

    add first component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return (
+      <div>
+        <h2>ExpenseItem</h2>
+        Food Rs 10 <br/>
+        Petrol Rs 100 <br/>
+        Movies Rs 200<br/>
+      </div>
+    );
+}
+
+export default ExpenseItem;
