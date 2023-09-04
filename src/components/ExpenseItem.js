c1c9a5c2c8b6348bff2d5885bba595147af43481
ssarun9871 <ssarun9871@gmail.com>
2023-09-05 01:22:12 +0530

    code modularization

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-us',{month:'long'});
-  const day = props.date.toLocaleString('en-us',{day:'2-digit'});
-  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-    <div>
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
-      </div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <h2>{props.location}</h2>
-        <div className="expense-item__price">{props.amount}</div>
-      </div>
+       <ExpenseDate date={props.date}/>
+       <ExpenseDetails title={props.title} location={props.location} amount={props.amount}/>
     </div>
   );
 }
