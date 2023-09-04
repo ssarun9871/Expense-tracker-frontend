9916d704e6231efabec050d52fa6216e56aabcfc
ssarun9871 <ssarun9871@gmail.com>
2023-09-05 00:54:57 +0530

    adding expense data and props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import ExpenseItem from "./components/ExpenseItem";
 
+const expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    location: "Delhi",
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12), location: 'Himanchal'},
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    location: "Dehradun",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    location: "Chandigarh",
+  },
+];
+
 function App() {
+  const expenseItems = expenses.map((element) => (
+    <ExpenseItem
+      key={element.id}
+      title={element.title}
+      amount={element.amount}
+      date={element.date}
+      location={element.location}
+    ></ExpenseItem>
+  ));
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem>
-      </ExpenseItem>
+      {expenseItems}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,23 @@
-function ExpenseItem(){
-    return (
-      <div>
-        <h2>ExpenseItem</h2>
-        Food Rs 10 <br/>
-        Petrol Rs 100 <br/>
-        Movies Rs 200<br/>
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-us',{month:'long'});
+  const day = props.date.toLocaleString('en-us',{day:'2-digit'});
+  const year = props.date.getFullYear();
+  return (
+    <div className="expense-item">
+    <div>
+      <div>{month}</div>
+      <div>{day}</div>
+      <div>{year}</div>
       </div>
-    );
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <h2>{props.location}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
