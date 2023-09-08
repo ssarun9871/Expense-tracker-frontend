f17b299b9e8391f3f1ed54c4e355bc0851b86c35
ssarun9871 <ssarun9871@gmail.com>
2023-09-09 00:50:35 +0530

    implemented useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,10 +2,14 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 import Card from '../UI/Card.js'
+import React, {useState} from "react";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
 
-  function onDeleteHandler(id){
+
+  const onDeleteHandler = (id) => {
     const element = document.getElementById(id);
     if(element){
       element.remove();
@@ -14,15 +18,27 @@ function ExpenseItem(props) {
       console.log("no-element found")
     }
   }
+
+  const onTitleChange = () => {
+    setTitle('update!');
+  }
+
+  const onAmounChange = () => {
+    setAmount('$100');
+  }
+  
   return (
     <Card className="expense-item" id={props.id}>
       <ExpenseDate date={props.date} />
       <ExpenseDetails
-        title={props.title}
+        title={title}
         location={props.location}
-        amount={props.amount}
+        amount={amount}
       />
+     
+      <button onClick={onTitleChange}>Change title</button>
       <button onClick={()=> onDeleteHandler(props.id)}>Delete</button>
+      <button onClick={onAmounChange}>change amount</button>
     </Card>
   );
 }
